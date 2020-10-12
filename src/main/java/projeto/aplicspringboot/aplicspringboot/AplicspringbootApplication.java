1f2c12e04210722d5bf5a1f7dcd18fce13107556
LorranPdS <lorransantospereira@yahoo.com.br>
2020-10-12 16:28:36 -0300

    Implementação básica de CRUD

---
+++ src/main/java/projeto/aplicspringboot/aplicspringboot/AplicspringbootApplication.java
@@ -0,0 +1,21 @@
+package projeto.aplicspringboot.aplicspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EntityScan(basePackages = "projeto.aplicspringboot.model")
+@ComponentScan(basePackages = { "projeto.*" })
+@EnableJpaRepositories(basePackages = { "projeto.aplicspringboot.repository" })
+@EnableTransactionManagement
+public class AplicspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/aplicspringboot/aplicspringboot/AplicspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.aplicspringboot.aplicspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
