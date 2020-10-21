9975142bde529b044699b254ee82bed35d49afc3
LorranPdS <lorransantospereira@yahoo.com.br>
2020-10-21 10:48:21 -0300

    Utilização do Spring Security

--- src/main/java/projeto/aplicspringboot/aplicspringboot/AplicspringbootApplication.java
+++ src/main/java/projeto/aplicspringboot/aplicspringboot/AplicspringbootApplication.java
@@ -4,18 +4,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "projeto.aplicspringboot.model")
 @ComponentScan(basePackages = "projeto.*")
 @EnableJpaRepositories(basePackages = "projeto.aplicspringboot.repository")
 @EnableTransactionManagement
-public class AplicspringbootApplication {
+@EnableWebMvc
+public class AplicspringbootApplication implements WebMvcConfigurer {
 
+	/*Esse EnableWebMvc serve para habilitarmos todos os 
+	 * recursos de MVC para ele poder redirecionar para nossa tela*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AplicspringbootApplication.class, args);
+		
+		/*
+		 * O código abaixo é para criar senhas criptografadas. Quando for utilizar ele,
+		 * comente todas as anotações acima, execute o código e será impressa
+		 * a senha criptografada. A seguir, copie a senha e cole no password
+		 * da classe WebConfigSecurity
+		 
+		 
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String senhaCriptografada = encoder.encode("123");
+		System.out.println(senhaCriptografada); */
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// Abaixo mapeamos e redirecionamos para a tela de login
+		registry.addViewController("/login").setViewName("/login");
+		registry.setOrder(Ordered.LOWEST_PRECEDENCE);
 	}
 
 }
